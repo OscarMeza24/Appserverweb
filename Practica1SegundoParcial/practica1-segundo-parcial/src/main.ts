c861bea14b43fe1c6132f9a2e07cafc85801d2e5
Oscar Meza <weynomames53@gmail.com>
2025-06-25 05:48:55 -0500

    Agregacion de comentario

--- Practica1SegundoParcial/practica1-segundo-parcial/src/main.ts
+++ Practica1SegundoParcial/practica1-segundo-parcial/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  //http://localhost:3000/api
+  //http://localhost:3000/api/voluntarios
   app.setGlobalPrefix('api');
   await app.listen(process.env.PORT ?? 3000);
 }
