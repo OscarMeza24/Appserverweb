70b22ba2de5a3a2156dec68b6f416d78487a7f4d
Oscar Meza <weynomames53@gmail.com>
2025-06-25 09:07:32 -0500

    Reinicio desde 0 de la practica

--- Practica1SegundoParcial/practica1-segundo-parcial/src/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { VoluntariosModule } from './voluntarios/voluntarios.module';
-
-@Module({
-  imports: [VoluntariosModule],
-  controllers: [],
-  providers: [],
-})
-export class AppModule {}
--- Practica1SegundoParcial/practica1-segundo-parcial/src/voluntarios/dto/update-voluntario.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateVoluntarioDto } from './create-voluntario.dto';
-
-export class UpdateVoluntarioDto extends PartialType(CreateVoluntarioDto) {}
