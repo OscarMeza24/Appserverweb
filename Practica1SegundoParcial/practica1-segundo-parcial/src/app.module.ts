321fe1a4099f543f8e36c90b3a5a20c7aff4b18e
Oscar Meza <weynomames53@gmail.com>
2025-06-25 05:46:41 -0500

    Creacion de recurso voluntarios y configuracion de la ruta

--- Practica1SegundoParcial/practica1-segundo-parcial/src/app.module.ts
+++ Practica1SegundoParcial/practica1-segundo-parcial/src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { VoluntariosModule } from './voluntarios/voluntarios.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [VoluntariosModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
